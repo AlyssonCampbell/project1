39bfd9ee013e65fb15cef9d1ef7887030858d1a8
Alysson Campbell <alyssoncampbell252@gmail.com>
2018-06-04 16:20:29 -0400

    have html and js running and set event listener for button

---
+++ main.js
@@ -0,0 +1,26 @@
+//what I need to do to get the hangman game running:
+//set an array with the word(s) that will be guessed
+//create an input field and a button to submit a guess from the user
+//create a second array where the user input letter will be stored
+//compare each input with all of the letters within the string in the array
+//if the letter input within the field matches any of the letters in the answer
+//then change the blank image with the ___ to the letter to display
+//do this maybe with a style/switch?
+//when the complete string has been input by the user
+//then give an alert or some other indication that the user won
+
+
+console.log("javascript running & ready to code! :D");
+
+//set an array with the word(s) that will be guessed
+const answerKey = ["testing"];
+//create an input field and a button to submit a guess from the user
+const guesses = [];
+//create event listenener for when submit button is clicked-input the letter input into the array
+const button = document.querySelector("button");
+const guessLetter = document.querySelector("#letter-guess");
+
+guessLetter.addEventListener("click", evt => {
+    evt.preventDefault();
+    console.log("testing the click!");
+});
