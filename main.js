9ed1036391677ecf3fa06909c86e0662eee0a81d
Alysson Campbell <alyssoncampbell252@gmail.com>
2018-06-04 21:59:54 -0400

    got comparison function working

--- main.js
+++ main.js
@@ -9,21 +9,31 @@
 //when the complete string has been input by the user
 //then give an alert or some other indication that the user won
 
-
 console.log("javascript running & ready to code! :D");
 
 //set an array with the word(s) that will be guessed
 const answerKey = ["abcdef"];
 //create an input field and a button to submit a guess from the user
 let guesses = [];
-//create event listenener for when submit button is clicked-input the letter input 
+//create event listenener for when submit button is clicked-input the letter input
 //into the guesses array
+//don't need to store the guesses within the array since each letter will be compared indivudually
 const button = document.querySelector("button");
 const guessLetter = document.querySelector("#letter-guess");
 
 guessLetter.addEventListener("click", evt => {
-    evt.preventDefault();
-    console.log("testing the click!");
-    guesses.push(guess.value);
-    console.log(guesses);
+  evt.preventDefault();
+  guesses.pop(guess.value);
+  guesses.push(guess.value);
+  console.log(guesses);
+  checkLetter(answerKey, guesses[0]);
 });
+
+//create function to compare each letter input to the answer array and see if a letter input is a
+//letter contained within the string
+function checkLetter(answerKey, guesses) {
+  for (var i = 0; i < answerKey.length; i++) {
+    if (answerKey[i] == guesses) console.log("match!");
+    return true;
+  }
+}
