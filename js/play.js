d6cf113e2ef0127d0a2c30f4f702fb9f2759581e
Alysson Campbell <alyssoncampbell252@gmail.com>
2018-07-18 21:21:51 -0400

    cleaned up functions and organized

--- js/play.js
+++ js/play.js
@@ -1,13 +1,11 @@
 "use strict";
 
-const button = document.querySelector("button");
 const guessLetter = document.querySelector("#letter-guess");
-const letterDisplay = document.querySelector(".letters");
 
 let guesses = [];
 let totalScore = 0;
 let wrongAnswers = 6;
-let indices = [];
+let letterPosition = [];
 let wrongLetters = [];
 
 //TODO:Improvements:make the input not function if empty array & convert to lowercase
@@ -33,18 +31,27 @@ function getLetter() {
   document.getElementById("guess").value = "";
 };
 
-//TODO: reduce the size of the checkLetter function
-//compare each input with all of the letters within the string in the array & pushes the information into the html
+//compare each input with all of the letters within the string in the array
 function checkLetter() {
   if (letterArray.includes(guesses[0]) == false) {
-    wrongAnswers = parseInt(wrongAnswers - 1);
-    wrongLetters.pop(guesses[0]);
-    wrongLetters.push(guesses[0]);
-    let incorrect = document.createElement("div");
-    incorrect.setAttribute("class", "wrong-letter");
-    document.querySelector(".wrong").appendChild(incorrect);
-    incorrect.innerText = wrongLetters;
+    storeWrong();
+    showWrong();
   };
+};
+
+//tracks the incorrect guessed letters
+function storeWrong() {
+  wrongAnswers = parseInt(wrongAnswers - 1);
+  wrongLetters.pop(guesses[0]);
+  wrongLetters.push(guesses[0]);
+};
+
+//displays wrong guesses on screen
+function showWrong() {
+  let incorrect = document.createElement("div");
+  incorrect.setAttribute("class", "wrong-letter");
+  document.querySelector(".wrong").appendChild(incorrect);
+  incorrect.innerText = wrongLetters;
 };
 
 //compares the input letter against all of the letters within the word being guessed
@@ -52,17 +59,22 @@ function searchWord() {
   for (let i in letterArray) {
     if (guesses[0] === letterArray[i]) {
       totalScore = parseInt(totalScore + 1);
-      letterArray[i] === guesses;
-      indices.pop(i);
-      indices.push(i);
+      storeIndex(i);
       showLetter();
     };
   };
 };
 
+//stores the index position of the letter if the user guesses a letter in the word
+function storeIndex(i) {
+  letterArray[i] === guesses;
+  letterPosition.pop(i);
+  letterPosition.push(i);
+};
+
 //displays the letter within the box when guessed correctly
 function showLetter() {
-  let blanks = document.getElementsByClassName("letter-display")[indices];
+  let blanks = document.getElementsByClassName("letter-display")[letterPosition];
   blanks.innerText = guesses;
 };
 
@@ -80,7 +92,7 @@ function win() {
   };
 };
 
-//if the attempts left hits 0 then game over
+//if the attempts left hits 0 and user lost
 function lost() {
   if (wrongAnswers === 0) {
     let message = ("UNAUTHORIZED USER DETECTED! SYSTEM RESETTING...");
@@ -88,6 +100,7 @@ function lost() {
   };
 };
 
+//game over alert function
 function gameOver(message) {
   setTimeout(function () {
     alert(message);
